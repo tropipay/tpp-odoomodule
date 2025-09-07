de5c1d3c53382a1f8df84f04c3a3e94775e59d72
Daniel Calderón <dani@danielcalderon.com>
2025-09-07 18:29:28 +0200

    addLogo

--- __manifest__.py
+++ __manifest__.py
@@ -19,6 +19,7 @@
     'website': "https://www.tropipay.com",
     'depends': ['payment', 'account', 'website', 'website_sale'],
     'data': [
+        'data/payment_icon_data.xml',
         'data/payment_method_data.xml',
         'views/payment_template.xml',
         'views/payment_tpp_templates.xml',
